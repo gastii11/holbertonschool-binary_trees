6870991dad51c608bb0efa67157ea6b401831b29
gastii11 <10632@holbertonstudents.com>
2025-04-07 17:11:18 +0000

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree-delete - borra un arbol binario completo
+ * @tree: puntero al nodo raiz del arbol a borrar
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	binary_tree_delete(tree->left);
+
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
