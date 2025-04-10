00cc77d4e9cf219fee64e9fe69a4762f8f524345
unixpup <dev@fault.wtf>
2025-04-10 10:09:02 -0300

    test part 364342524

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+
+/**
+ * height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ *
+ * Return: The height of the tree, 0 if tree is NULL
+ */
+size_t height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the balance factor
+ *
+ * Return: The balance factor of the tree, 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = (int)height(tree->left);
+	right_height = (int)height(tree->right);
+
+	return (left_height - right_height);
+}
